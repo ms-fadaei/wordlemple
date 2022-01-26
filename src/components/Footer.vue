7eaba48767ced205ce92da2b6c1b601c1c154c0b
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-01-26 12:35:29 +0330

    chore: update github repo url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,17 +3,31 @@ import { isDark, toggleDark } from '~/composables'
 </script>
 
 <template>
-  <nav text-xl mt-6 inline-flex gap-2>
-    <button class="icon-btn !outline-none" @click="toggleDark()">
-      <div v-if="isDark" i-carbon-moon />
-      <div v-else i-carbon-sun />
+  <nav
+    text-xl
+    mt-6
+    inline-flex
+    gap-2
+  >
+    <button
+      class="icon-btn !outline-none"
+      @click="toggleDark()"
+    >
+      <div
+        v-if="isDark"
+        i-carbon-moon
+      />
+      <div
+        v-else
+        i-carbon-sun
+      />
     </button>
 
     <a
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/ms-fadaei/wordlemple"
       target="_blank"
       title="GitHub"
     />
