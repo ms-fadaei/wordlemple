e582978c1fbe07e5f1c209e1094badbfe632c2e6
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-01-26 08:49:01 +0330

    fix: remove vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { presetAttributify, presetIcons, presetUno } from 'unocss'
@@ -17,9 +16,6 @@ export default defineConfig({
   },
   plugins: [
     Vue(),
-
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages(),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
