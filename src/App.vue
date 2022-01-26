f2ddef12f2f396d73c6063db497e6a7537c42ab7
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-01-26 23:41:20 +0330

    feat: add title

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     un-bg="gray-50 dark:gray-900"
     un-text="gray-700 dark:gray-200"
   >
+    <h1 class="mb-3 text-cool-gray-600 dark:text-cool-gray-400 text-xl">WORDLEMPLE</h1>
     <Wordlemple :word="word" :tries="5" class="p-4 rounded-2" un-bg="gray-100 dark:gray-800" />
     <Footer />
   </main>
